b1961e1101c29fbd5c9f720b4ba4e0a84acefcdc
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2026-05-03 20:58:23 +0900

    index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { default as applyStyleProps } from './applyStyleProps';
+export type * from './applyStyleProps/types';
 export * from './constants';
 export type * from './types';
 export { default as withStyleProps } from './withStyleProps';
+export type * from './withStyleProps/types';
